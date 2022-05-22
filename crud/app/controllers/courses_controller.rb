26750bf0cdffc77074eff3d344cc298b658d5a48
g1alexander <alexlds26@gmail.com>
2022-05-22 16:52:52 -0500

    crud complete

--- crud/app/controllers/courses_controller.rb
+++ crud/app/controllers/courses_controller.rb
@@ -1,4 +1,6 @@
 class CoursesController < ApplicationController
+  before_action :set_course, only: %i[show edit update destroy]
+
   def index
     @courses = Course.all
   end
@@ -12,9 +14,27 @@ class CoursesController < ApplicationController
     redirect_to courses_path
   end
 
+  def show; end
+
+  def edit; end
+
+  def update
+    @course.update course_params
+    redirect_to course_path
+  end
+
+  def destroy
+    @course.destroy
+    redirect_to courses_path, status: :see_other
+  end
+
   private
 
   def course_params
     params.required(:course).permit(:title, :description, :visible, :published)
+  end
+
+  def set_course
+    @course = Course.find params[:id]
   end
 end
--- crud/config/routes.rb
+++ crud/config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   get 'courses/', to: 'courses#index'
   get 'courses/new'
 
+  get 'courses/:id', to: 'courses#show', as: 'show_course'
+  get 'courses/:id/edit', to: 'courses#edit', as: 'edit_course'
+
   post 'courses', to: 'courses#create'
+  patch 'courses/:id', to: 'courses#update', as: 'course'
+  delete 'courses/:id', to: 'courses#destroy', as: 'course_delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
